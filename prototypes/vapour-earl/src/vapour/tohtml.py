677d4cc908a3bdebca0812cd4ad2aecfef92e27f
berrueta <berrueta@localhost>
2007-02-20 19:19:33 +0000

    Prototype of Python report generator

---
+++ prototypes/vapour-earl/src/vapour/tohtml.py
@@ -0,0 +1,67 @@
+from rdflib import Graph, Namespace
+from rdflib.sparql import sparqlGraph
+from rdflib.sparql.graphPattern import GraphPattern
+
+EARL = Namespace("http://www.w3.org/WAI/ER/EARL/nmg-strawman#")
+RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+DC = Namespace("http://purl.org/dc/elements/1.1/")
+URI = Namespace("http://www.w3.org/2006/uri#")
+HTTP = Namespace("http://www.w3.org/2006/http#")
+
+store = Graph()
+
+store.parse("http://localhost/~berrueta/vapour.rdf")
+store.parse("http://localhost/~berrueta/recipes.rdf")
+store.parse("http://localhost/~berrueta/earl.rdf")
+
+store.parse("/home/berrueta/scratch/svn/vapour/trunk/webpage/demo-report1.rdf")
+
+sparqlGr = sparqlGraph.SPARQLGraph(store)
+
+#for s, p, o in store: print s, p, o
+
+print """
+<html>
+  <head>
+    <title>Vapour Report</title>
+  </head>
+  <body>
+    <h1>Vapour Report</h1>
+"""
+
+print "<h2>Test results</h2>"
+select = ("?assertion", "?test", "?testTitle", "?validity", "?validityLabel")
+where = GraphPattern([
+                      ("?assertion", RDF["type"], EARL["Assertion"]),
+                      ("?assertion", EARL["test"], "?test"),
+                      ("?test", DC["title"], "?testTitle"),
+                      ("?assertion", EARL["result"], "?result"),
+                      ("?result", EARL["validity"], "?validity"),
+                      ("?validity", RDFS["label"], "?validityLabel")
+                     ])
+print "<ul>"
+for row in sparqlGr.query(select, where):
+    print "<li>", row[2], ":", row[4], "</li>"
+print "</ul>"
+
+print "<h2>HTTP trace</h2>"
+select = ("?testSub", "?testSubTitle", "?uri", "?responseCode", "?responseContentType")
+where = GraphPattern([
+                      ("?testSub", RDF["type"], EARL["TestSubject"]),
+                      ("?testSub", DC["title"], "?testSubTitle"),
+                      ("?testSub", EARL["httpRequest"], "?request"),
+                      ("?request", URI["uri"], "?uri"),
+                      ("?testSub", EARL["httpResponse"], "?response"),
+                      ("?response", HTTP["responseCode"], "?responseCode"),
+                      ("?response", HTTP["content-type"], "?responseContentType")
+                      ])
+print "<ul>"
+for row in sparqlGr.query(select, where):
+    print "<li>", row[1], ":", row[2], "(", row[3], ",", row[4], ")",  "</li>"
+print "<ul>"
+
+print """
+  </body>
+</html>
+"""
