6d457b9c7de52cf913cc2117e7d6101a77a09040
Diego Berrueta <diego@berrueta.net>
2007-02-26 18:38:40 +0000

    Added common namespaces declaration

---
+++ apps/vapour/src/vapour/namespaces.py
@@ -0,0 +1,8 @@
+from rdflib import Namespace
+
+EARL = Namespace("http://www.w3.org/WAI/ER/EARL/nmg-strawman#")
+RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+DC = Namespace("http://purl.org/dc/elements/1.1/")
+URI = Namespace("http://www.w3.org/2006/uri#")
+HTTP = Namespace("http://www.w3.org/2006/http#")
