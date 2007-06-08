93d97fdd2e39a0647c078477d2dd1319be050a96
Diego Berrueta <diego@berrueta.net>
2007-06-08 14:02:47 +0000

    Added new command line options

--- apps/vapour/run-vapour-cmd.sh
+++ apps/vapour/run-vapour-cmd.sh
@@ -7,14 +7,49 @@ export PYTHONPATH=$VAPOUR_BASE_DIR/src:$PYTHONPATH
 export VAPOUR_RDF_FILES=$VAPOUR_BASE_DIR/../../webpage
 export VAPOUR_TEMPLATES=$VAPOUR_BASE_DIR/src/vapour/strainer/templates
 
+export OUTPUT_DIR=../../webpage
+
+# Recipe 1
 python2.4 src/vapour/cup/cmdline.py \
     1 \
-    "http://vapour.sourceforge.net/recipes-web/example1" \
-    "http://vapour.sourceforge.net/recipes-web/example1#Concept" \
-    "http://vapour.sourceforge.net/recipes-web/ERRATA/example1#prefLabel"
+    "http://localhost/~berrueta/recipes-web/example1" \
+    "http://localhost/~berrueta/recipes-web/example1#Concept" \
+    "http://localhost/~berrueta/recipes-web/ERRATA/example1#prefLabel" \
+    $OUTPUT_DIR/recipe1.html \
+    $OUTPUT_DIR/recipe1.rdf
 
+# Recipe 2
 python2.4 src/vapour/cup/cmdline.py \
     2 \
-    "http://isegserv.itd.rl.ac.uk/VM/http-examples/example2/" \
-    "http://isegserv.itd.rl.ac.uk/VM/http-examples/example2/ClassA" \
-    "http://isegserv.itd.rl.ac.uk/VM/http-examples/example2/propB"
+    "http://localhost/~berrueta/recipes-web/example2/" \
+    "http://localhost/~berrueta/recipes-web/example2/ClassA" \
+    "http://localhost/~berrueta/recipes-web/example2/propB" \
+    $OUTPUT_DIR/recipe2.html \
+    $OUTPUT_DIR/recipe2.rdf
+
+# Recipe 3
+python2.4 src/vapour/cup/cmdline.py \
+    3 \
+    "http://localhost/~berrueta/recipes-web/example3" \
+    "http://localhost/~berrueta/recipes-web/example3#Concept" \
+    "http://localhost/~berrueta/recipes-web/example3#prefLabel" \
+    $OUTPUT_DIR/recipe3.html \
+    $OUTPUT_DIR/recipe3.rdf
+
+# Recipe 4
+python2.4 src/vapour/cup/cmdline.py \
+    4 \
+    "http://localhost/~berrueta/recipes-web/example4/" \
+    "http://localhost/~berrueta/recipes-web/example4/Concept" \
+    "http://localhost/~berrueta/recipes-web/example4/prefLabel" \
+    $OUTPUT_DIR/recipe4.html \
+    $OUTPUT_DIR/recipe4.rdf
+
+# Recipe 5
+python2.4 src/vapour/cup/cmdline.py \
+    5 \
+    "http://localhost/~berrueta/recipes-web/example5/" \
+    "http://localhost/~berrueta/recipes-web/example5/Concept" \
+    "http://localhost/~berrueta/recipes-web/example5/prefLabel" \
+    $OUTPUT_DIR/recipe5.html \
+    $OUTPUT_DIR/recipe5.rdf    
--- apps/vapour/src/vapour/cup/cmdline.py
+++ apps/vapour/src/vapour/cup/cmdline.py
@@ -21,14 +21,16 @@ if __name__ == "__main__":
     vocabUri = sys.argv[2]
     classUri = sys.argv[3]
     propUri = sys.argv[4]
+    outputFileName = sys.argv[5]
+    outputRdfFileName = sys.argv[6]
     
     store = Graph()
-    store.parse(pathToRdfFiles + "/vapour.rdf")
-    store.parse(pathToRdfFiles + "/recipes.rdf")
-    store.parse(pathToRdfFiles + "/earl.rdf")
     
     recipesFunctions = { 1 : recipes.recipe1,
-                         2 : recipes.recipe2
+                         2 : recipes.recipe2,
+                         3 : recipes.recipe3,
+                         4 : recipes.recipe4,
+                         5 : recipes.recipe5
                        }
     
     if recipeNumber in recipesFunctions:
@@ -38,6 +40,21 @@ if __name__ == "__main__":
         print "ERROR: Unknown recipe number: " + recipeNumber
         sys.exit(-1)
     
+    if outputRdfFileName is not None:
+        outputRdfFile = open(outputRdfFileName,'w')
+        outputRdfFile.write(store.serialize(format="pretty-xml"))
+        outputRdfFile.close()
+
+    store.parse(pathToRdfFiles + "/vapour.rdf")
+    store.parse(pathToRdfFiles + "/recipes.rdf")
+    store.parse(pathToRdfFiles + "/earl.rdf")
+
     model = sparqlGraph.SPARQLGraph(store)
     html = strainer.resultsModelToHTML(model, pathToTemplates)
-    print html
+    if outputFileName is not None:
+        outputFile = open(outputFileName,'w')
+        outputFile.write(str(html))
+        outputFile.close()
+    else:
+        print html
+        
