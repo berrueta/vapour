f6ea7aae68f4e215f6c12b41d0d63cebe40807cb
Diego Berrueta <diego@berrueta.net>
2007-07-25 11:27:58 +0000

    Refactored tests

--- apps/vapour/src/vapour/teapot/mimetypes.py
+++ apps/vapour/src/vapour/teapot/mimetypes.py
@@ -4,22 +4,18 @@ xhtml = "application/xhtml+xml"
 
 xhtmlOrHtml = xhtml + "," + html
 
-qualifiedRdfXml = "application/rdf+xml;q=0.5"
-qualifiedHtml = "text/html;q=0.5"
-qualifiedXhtml = "application/xhtml+xml;q=0.5"
-
 mixed = [ "application/rdf+xml;q=0.5,text/html;q=.3",
  "application/rdf+xml;q=0.3,text/html;q=.5",
  "application/rdf+xml;q=0.5,text/html;q=.5",
  rdfXml + "," + html,
- html + "," + rdfXml ]
+ html + "," + rdfXml,
+ rdfXml + ";q=0.5",
+ html + ";q=0.5",
+ xhtml + ";q=0.5"  ]
 
 requestDescription = {
                rdfXml : "requesting RDF/XML data",
                html : "requesting HTML data",
                xhtml : "requesting XHTML data",
-               xhtmlOrHtml : "requesting (X)HTML data",
-               qualifiedRdfXml : "requesting RDF/XML with 'q' value",
-               qualifiedHtml : "requesting HTML with 'q' value",
-               qualifiedXhtml : "requesting XHTML with 'q' value"
+               xhtmlOrHtml : "requesting (X)HTML data"
                }
