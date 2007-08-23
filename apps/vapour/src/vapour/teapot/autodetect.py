7926b2a12f09561056dbddd005882dcbe8972341
Diego Berrueta <diego@berrueta.net>
2007-08-23 18:28:20 +0000

    Better exception management

--- apps/vapour/src/vapour/teapot/autodetect.py
+++ apps/vapour/src/vapour/teapot/autodetect.py
@@ -7,28 +7,28 @@ from vapour.namespaces import RDF, RDFS, OWL
 def autodetectUris(graph, vocabUri):
     contentType = mimetypes.rdfXml
     try:
-        (rootTestSubject, response) = followRedirects(graph, "Derreferencing the vocabulary URI", vocabUri, contentType)
-        if response.status == httplib.OK:
+        ( rootTestSubject, response ) = followRedirects( graph, "Derreferencing the vocabulary URI", vocabUri, contentType )
+    except Exception, e:
+        raise Exception( "Unable to autodetect URIs, the vocabulary cannot be retrieved (inner exception=" + str(e) + ")" )
+
+    if response.status == httplib.OK:
             tempGraph = ConjunctiveGraph()
-            tempGraph.load(response)
+            tempGraph.load( response )
             
-            rdfsClasses = [x for x in tempGraph.subjects(RDF["type"], RDFS["Class"])]
-            owlClasses = [x for x in tempGraph.subjects(RDF["type"], OWL["Class"])]        
-            rdfProperties = [x for x in tempGraph.subjects(RDF["type"], RDF["Property"])]
-            objectProperties = [x for x in tempGraph.subjects(RDF["type"], OWL["ObjectProperty"])]
-            datatypeProperties = [x for x in tempGraph.subjects(RDF["type"], OWL["DatatypeProperty"])]
-            annotationProperties = [x for x in tempGraph.subjects(RDF["type"], OWL["AnnotationProperty"])]
+            rdfsClasses = [x for x in tempGraph.subjects( RDF["type"], RDFS["Class"] )]
+            owlClasses = [x for x in tempGraph.subjects( RDF["type"], OWL["Class"] )]        
+            rdfProperties = [x for x in tempGraph.subjects( RDF["type"], RDF["Property"] )]
+            objectProperties = [x for x in tempGraph.subjects( RDF["type"], OWL["ObjectProperty"] )]
+            datatypeProperties = [x for x in tempGraph.subjects( RDF["type"], OWL["DatatypeProperty"] )]
+            annotationProperties = [x for x in tempGraph.subjects( RDF["type"], OWL["AnnotationProperty"] )]
             
             # remove unwanted URIs
-            owlClasses = filter(lambda x : not x.startswith(OWL), owlClasses)
+            owlClasses = filter( lambda x : not x.startswith( OWL ), owlClasses )
     
             classes = rdfsClasses + owlClasses
             properties = rdfProperties + objectProperties + datatypeProperties + annotationProperties
-            return (classes, properties)
-        else:
-            print "WARNING: Unable to autodetect URIs, the vocabulary cannot be retrieved (response code=", str(response.status), ")"
-            return (None, None)
-    except Exception, e:
-        print "WARNING: Unable to autodetect URIs, the vocabulary cannot be retrieved (exception=", e, ")"
-        return (None, None)
+            return ( classes, properties )
+    else:
+            raise Exception( "Unable to autodetect URIs, the vocabulary cannot be retrieved (response code=" + str( response.status ) + ")" )
+
         
