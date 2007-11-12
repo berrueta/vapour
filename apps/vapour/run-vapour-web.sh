f1820dba5e0323bc640c983e77703826b099628d
berrueta <berrueta@localhost>
2007-11-12 12:45:51 +0000

    Set value for the VAPOUR_LOG variable

--- apps/vapour/run-vapour-web.sh
+++ apps/vapour/run-vapour-web.sh
@@ -6,6 +6,7 @@ export PYTHONPATH=$VAPOUR_BASE_DIR/src:$PYTHONPATH
 
 export VAPOUR_RDF_FILES=$VAPOUR_BASE_DIR/../../webpage
 export VAPOUR_TEMPLATES=$VAPOUR_BASE_DIR/src/vapour/strainer/templates
+export VAPOUR_LOG=$VAPOUR_BASE_DIR/log.txt
 
 #export OUTPUT_DIR=../../webpage
 
