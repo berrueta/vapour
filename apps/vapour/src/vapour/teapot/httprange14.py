b29ea600de23b17cf4f65d1e30ad954f08cdc120
berrueta <berrueta@localhost>
2008-06-29 10:38:20 +0000

    httpRange-14 does not provide information about hashed URIs, because they are not directly de-referenceable

--- apps/vapour/src/vapour/teapot/httprange14.py
+++ apps/vapour/src/vapour/teapot/httprange14.py
@@ -1,25 +1,30 @@
 from util import *
 from vapour.namespaces import *
-import httplib
+import httplib, urlparse
 from rdflib import URIRef
 
 # NOTE: The resolution of the httpRange-14 issue [1] covers just
 # HTTP 'GET' requests. Nothing is said about 'HEAD' requests. However
 # the HTTP specification [2] says that 'HEAD' requests are "identical to
 # GET except that the server MUST NOT return a message-body in
-#Êthe response". Therefore, we assume that the conclusions of
+# the response". Therefore, we assume that the conclusions of
 # httpRange-14 still hold for 'HEAD' requests.
 #
-#Ê[1] http://lists.w3.org/Archives/Public/www-tag/2005Jun/0039.html
+# [1] http://lists.w3.org/Archives/Public/www-tag/2005Jun/0039.html
 # [2] http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.4
 
 def httpRange14Conclusions(graph, rootTestSubject):
     testSubjects = testSubjectsAsList(graph, rootTestSubject)
     for testSubject in testSubjects:
         requestUri = getRequestUri(graph, testSubject)
+        parsedUri = urlparse.urlparse(requestUri) # (protocol,server,path,params,query,fragment)
+        actuallyRequestedUri = urlparse.urlunparse((parsedUri[0], parsedUri[1], parsedUri[2], parsedUri[3], parsedUri[4], ""))
+        if parsedUri[5] is not "":
+            graph.add( (testSubject, VAPOUR_VOCAB["httpRange14ConclusionOn"], URIRef(requestUri) ) )
+            graph.add( (URIRef(requestUri), RDF["type"], VAPOUR_VOCAB["AnyResource"] ) )
         responseCode = getResponseCode(graph, testSubject)
-        graph.add((testSubject, VAPOUR_VOCAB["httpRange14ConclusionOn"], URIRef(requestUri)))
+        graph.add((testSubject, VAPOUR_VOCAB["httpRange14ConclusionOn"], URIRef(actuallyRequestedUri)))
         if responseCode == httplib.OK:
-            graph.add((URIRef(requestUri), RDF["type"], VAPOUR_VOCAB["InformationResource"]))
+            graph.add((URIRef(actuallyRequestedUri), RDF["type"], VAPOUR_VOCAB["InformationResource"]))
         elif responseCode == httplib.SEE_OTHER:
-            graph.add((URIRef(requestUri), RDF["type"], VAPOUR_VOCAB["AnyResource"]))
+            graph.add((URIRef(actuallyRequestedUri), RDF["type"], VAPOUR_VOCAB["AnyResource"]))
