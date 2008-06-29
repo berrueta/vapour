e8a4b6c54df888e378711b89c7f4ae7051b08c82
Diego Berrueta <diego@berrueta.net>
2008-06-29 10:15:44 +0000

    Added comment

--- apps/vapour/src/vapour/teapot/httprange14.py
+++ apps/vapour/src/vapour/teapot/httprange14.py
@@ -3,6 +3,15 @@ from vapour.namespaces import *
 import httplib
 from rdflib import URIRef
 
+# NOTE: The resolution of the httpRange-14 issue [1] covers just
+# HTTP 'GET' requests. Nothing is said about 'HEAD' requests. However
+# the HTTP specification says that 'HEAD' requests are "identical to
+# GET except that the server MUST NOT return a message-body in
+#Êthe response". Therefore, we assume that the conclusions of
+# httpRange-14 still hold for 'HEAD' requests.
+#
+#Ê[1] http://lists.w3.org/Archives/Public/www-tag/2005Jun/0039.html
+
 def httpRange14Conclusions(graph, rootTestSubject):
     testSubjects = testSubjectsAsList(graph, rootTestSubject)
     for testSubject in testSubjects:
