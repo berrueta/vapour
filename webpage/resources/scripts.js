22de83702485b17f9907d1c03c2530366526f5f3
wikier <wikier@localhost>
2008-07-07 18:27:38 +0000

    fixed an accessibility issue

--- webpage/resources/scripts.js
+++ webpage/resources/scripts.js
@@ -8,6 +8,16 @@ function example() {
 	node.onclick = function() { document.getElementById("vocabUri").value=this.innerHTML };
 }
 
+function showHideForm() {
+    //vocabulary validation legend
+	var vocabularyValidationLegend = document.getElementById("vocabularyValidationLegend");
+	vocabularyValidationLegend.onclick = function() { showHide(vocabularyValidationLegend ,document.getElementById('vocabularyValidationSubform')); };
+
+    //vocabulary validation legend
+	var advancedOptionsLegend = document.getElementById("advancedOptionsLegend");
+	advancedOptionsLegend.onclick = function() { showHide(advancedOptionsLegend ,document.getElementById('advancedOptionsSubform')); };
+}
+
 function addLoadEvent(func) {
 	//by Simon Willison:
 	//   http://simon.incutio.com/archive/2004/05/26/addLoadEvent
@@ -24,4 +34,5 @@ function addLoadEvent(func) {
 }
 
 addLoadEvent(example);
+addLoadEvent(showHideForm);
 
