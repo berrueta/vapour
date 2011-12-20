e0b4a93e1d36dc914bf2885e850391a0c1728a66
Sergio Fernandez <sergio.fernandez@fundacionctic.org>
2011-12-20 12:58:35 +0100

    back to a common django app

--- apps/vapour/urls.py
+++ apps/vapour/urls.py
@@ -1,21 +1,12 @@
-# Copyright 2008 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-import settings
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, include, url
 from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect
 from vapour.cup.django.cup import GET
+import settings
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
 
 urlpatterns = patterns('',
     (r"^favicon.ico",   lambda request: HttpResponseRedirect(settings.MEDIA_URL+"images/favicon.ico")),
