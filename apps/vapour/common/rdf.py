3afa65f7dfda5c9b2233bfe4f78b5c51e62f4fa5
Sergio Fernandez <sergio.fernandez@fundacionctic.org>
2011-12-21 17:16:04 +0100

    allowing custom log on queries

--- apps/vapour/common/rdf.py
+++ apps/vapour/common/rdf.py
@@ -5,11 +5,13 @@ rdflib.plugin.register("sparql", rdflib.query.Result, "rdfextras.sparql.query", 
 from vapour.namespaces import bindings
 from vapour.cup.common import createLogger
 
-def performSparqlQuery(graph, query):
-    logger = createLogger()
-    logger.debug("Performing SPARQL query: %s" % normalizeQuery(query))
+def performSparqlQuery(graph, query, log=False):
+    if (log):
+        logger = createLogger()
+        logger.debug("Performing SPARQL query: %s" % normalizeQuery(query))
     results = graph.query(query, initNs=bindings)
-    logger.debug("Returned %d results" % len(results))
+    if (log):
+        logger.debug("Returned %d results" % len(results))
     return results
 
 def normalizeQuery(query):
