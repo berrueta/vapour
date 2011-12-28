e50c7de5f04db67b47d480eeca19407cc42a9a29
Sergio Fernandez <sergio.fernandez@fundacionctic.org>
2011-12-28 12:03:34 +0100

    fixed typo on path

--- apps/vapour/settings.py
+++ apps/vapour/settings.py
@@ -48,7 +48,7 @@ MEDIA_ROOT = os.path.join(ROOT_PATH, "..", "..", "webpage", "resources")
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = "/resouces/"
+MEDIA_URL = "/resources/"
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
