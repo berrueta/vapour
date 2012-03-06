b1b8b9aba90271b0771d59156e9cbb571d1d8b54
Sergio Fernandez <sergio.fernandez@fundacionctic.org>
2012-03-06 15:29:03 +0100

    added new methods to know if the test suite is passed or not

--- apps/api/java/src/main/java/net/sf/vapour/api/VapourApiMain.java
+++ apps/api/java/src/main/java/net/sf/vapour/api/VapourApiMain.java
@@ -7,9 +7,11 @@ public class VapourApiMain {
 		VapourApi api = VapourApiFactory.createVapourApi();
 		api.enableCacheDump();
 		VapourReport report = api.check(uri);
+		System.out.println();
 		System.out.println("Vapour Report:");
-		System.out.println("-------------\n");
-		System.out.println("URI: " + uri + "\n");
+		System.out.println("-------------");
+		System.out.println("URI: " + uri + "");
+		System.out.println("Result: " + report + "");
 		System.out.println("Tests performed: " + report.getTestPerformed());
 		System.out.println("Tests passed: " + report.getTestPassed());
 		System.out.println("Tests failed: " + report.getTestFailed());
--- apps/api/java/src/main/java/net/sf/vapour/api/VapourReportImpl.java
+++ apps/api/java/src/main/java/net/sf/vapour/api/VapourReportImpl.java
@@ -18,21 +18,30 @@ public class VapourReportImpl implements VapourReport {
 		this.model = model;
 		log.debug("Report created with " + model.size() + " statements");
 	}
+	
+	public boolean isValid() {
+		return !SparqlHelper.execAskQuery(this.model, QueryBuilder.buildAskFailedTests());
+	}	
 
 	public int getTestPerformed() {
-		return SparqlHelper.execCountQuery(this.model, QueryBuilder.buildTestsCount());
+		return SparqlHelper.execCountQuery(this.model, QueryBuilder.buildCountTests());
 	}
 
 	public int getTestPassed() {
-		return SparqlHelper.execCountQuery(this.model, QueryBuilder.buildPassedTestsCount());
+		return SparqlHelper.execCountQuery(this.model, QueryBuilder.buildCountPassedTests());
 	}
 	
 	public int getTestFailed() {
-		return SparqlHelper.execCountQuery(this.model, QueryBuilder.buildFailedTestsCount());
+		return SparqlHelper.execCountQuery(this.model, QueryBuilder.buildCountFailedTests());
 	}
 
 	public List<VapourTest> getTests() {
 		throw new RuntimeException("Unimplemented");
-	}	
+	}
+
+	@Override
+	public String toString() {
+		return (this.isValid() ? "all tests passed" : "some tests failed") + " (" + this.getTestPassed() + "/" + this.getTestPerformed() + ")";
+	}
 	
 }
