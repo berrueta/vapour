87a3eef7964819020233f9ebff2a9f8b1c8ac4f1
Sergio Fernández <sergio@wikier.org>
2012-03-06 13:34:28 +0100

    refactorized query execution

--- apps/api/java/src/main/java/net/sf/vapour/api/VapourReportImpl.java
+++ apps/api/java/src/main/java/net/sf/vapour/api/VapourReportImpl.java
@@ -2,12 +2,10 @@ package net.sf.vapour.api;
 
 import java.util.List;
 
+import net.sf.vapour.api.helpers.SparqlHelper;
+
 import org.apache.log4j.Logger;
 
-import com.hp.hpl.jena.query.QueryExecution;
-import com.hp.hpl.jena.query.QueryExecutionFactory;
-import com.hp.hpl.jena.query.QueryFactory;
-import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.rdf.model.Model;
 
 public class VapourReportImpl implements VapourReport {
@@ -23,32 +21,21 @@ public class VapourReportImpl implements VapourReport {
 
 	public int getTestPerformed() {
 		String query = "PREFIX earl: <http://www.w3.org/ns/earl#> SELECT (count(*) as ?count) WHERE { ?testRequirement a earl:TestRequirement }";
-		return this.execCountQuery(query);
+		return SparqlHelper.execCountQuery(this.model, query);
 	}
 
 	public int getTestPassed() {
 		String query = "PREFIX earl: <http://www.w3.org/ns/earl#> SELECT (count(*) as ?count) WHERE { ?assertion a earl:Assertion . ?assertion earl:result ?result . ?result earl:outcome earl:passed }";
-		return this.execCountQuery(query);
+		return SparqlHelper.execCountQuery(this.model, query);
 	}
 	
 	public int getTestFailed() {
 		String query = "PREFIX earl: <http://www.w3.org/ns/earl#> SELECT (count(*) as ?count) WHERE { ?assertion a earl:Assertion . ?assertion earl:result ?result . ?result earl:outcome earl:failed }";
-		return this.execCountQuery(query);
+		return SparqlHelper.execCountQuery(this.model, query);
 	}
 
 	public List<VapourTest> getTests() {
 		throw new RuntimeException("Unimplemented");
 	}	
 	
-	private int execCountQuery(String query) {
-		return this.execCountQuery(query, "count");
-	}
-	
-	private int execCountQuery(String query, String var) {
-		log.trace("Query: " + query);
-		QueryExecution qe = QueryExecutionFactory.create(QueryFactory.create(query), this.model);
-		ResultSet results = qe.execSelect();
-		return results.nextSolution().getLiteral(var).getInt();
-	}
-
 }
---
+++ apps/api/java/src/main/java/net/sf/vapour/api/helpers/SparqlHelper.java
@@ -0,0 +1,26 @@
+package net.sf.vapour.api.helpers;
+
+import org.apache.log4j.Logger;
+
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Model;
+
+public class SparqlHelper {
+	
+	private static final Logger log = Logger.getLogger(SparqlHelper.class);
+	
+	public static int execCountQuery(Model model, String query) {
+		return execCountQuery(model, query, "count");
+	}
+	
+	public static int execCountQuery(Model model, String query, String var) {
+		log.trace("Query: " + query);
+		QueryExecution qe = QueryExecutionFactory.create(QueryFactory.create(query), model);
+		ResultSet results = qe.execSelect();
+		return results.nextSolution().getLiteral(var).getInt();
+	}
+
+}
