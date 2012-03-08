b5edf0890bbb75a9842ca7db0f2a88ff88cf607a
Sergio Fernández <sergio@wikier.org>
2012-03-08 17:36:54 +0100

    added license header on the source files

--- apps/api/java/src/main/java/net/sf/vapour/api/VapourTestImpl.java
+++ apps/api/java/src/main/java/net/sf/vapour/api/VapourTestImpl.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (C) 2012 Fundación CTIC <http://fundacionctic.org>, All Rights Reserved.
+ *
+ * This work is distributed under the W3C® Software License in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * You may obtain a copy of the License at
+ *
+ *     http://www.w3.org/Consortium/Legal/2002/copyright-software-20021231
+ */
 package net.sf.vapour.api;
 
 import java.util.Collections;
