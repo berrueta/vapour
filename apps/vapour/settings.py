8c62cfbbfc9b7b5f4d922210156816e89f7c170b
Diego Berrueta <diego@berrueta.net>
2013-05-05 19:02:38 +1000

    Moved root application directory, moved static files, moved cached RDF files

--- apps/vapour/settings.py
+++ apps/vapour/settings.py
@@ -43,12 +43,12 @@ USE_I18N = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(ROOT_PATH, "..", "..", "webpage", "resources")
+MEDIA_ROOT = os.path.join(ROOT_PATH, "..", "media")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = "/resources/"
+MEDIA_URL = "/media/"
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -70,6 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(ROOT_PATH, "..", "static"),
 )
 
 # List of finder classes that know how to find static files in
@@ -163,11 +164,7 @@ ALLOWED_HOSTS = []
 
 #Custom stuff for vapour
 REQ_BASE_URL = "http://validator.linkeddata.org/vapour#req"
-if DEBUG:
-    PATH_RDF_FILES = os.path.join(ROOT_PATH, "..", "..", "webpage")
-else:
-    PATH_RDF_FILES = "http://vapour.sourceforge.net"
-PATH_RESOURCES_FILES = "http://vapour.sourceforge.net/resources"
+PATH_RDF_FILES = os.path.join(ROOT_PATH, "..", "rdf")
 PATH_TEMPLATES = os.path.join(ROOT_PATH, "strainer/templates/")
 ALLOW_INTRANET = True
 
